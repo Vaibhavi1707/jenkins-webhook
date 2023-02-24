839954db20faccd0b11773118746afa57e196b26
Vaibhavi Lokegaonkar <53178543+Vaibhavi1707@users.noreply.github.com>
2023-02-24 17:49:26 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -2,3 +2,6 @@ print("Hello World using web hooks with jenkins")
 print("Webhooks is working, if this builds")
 print("Webhooks works!")
 print("Webhooks workss!!")
+
+
+print("Pipelined, webhooked project")
